906b2654a857f47878840b3fea760b227dcf6508
Jozef Piecyk <jozollo@gmail.com>
2021-06-06 12:02:37 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/springbootswaggerexample/SpringbootSwaggerExampleApplication.java
@@ -0,0 +1,15 @@
+package pl.bykowski.springbootswaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerExampleApplication.class, args);
+    }
+
+
+
+}
